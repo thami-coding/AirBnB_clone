b4b5bc241616667c3e91681f8db4ac2e40aeab24
Thami <sainttsquared@gmail.com>
2022-11-27 23:41:52 +0200

    base_model.py constructor

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,14 @@ class BaseModel:
     attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        dt = datetime.fromisoformat(value)
+                        value = dt
+                    setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
