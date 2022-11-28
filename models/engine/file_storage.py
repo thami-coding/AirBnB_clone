f5dff92977e33a20b8a31d90ac8914dd2d55f6a7
Thami <sainttsquared@gmail.com>
2022-11-28 17:35:38 +0200

    corrections for file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,32 +16,14 @@ class FileStorage:
     FileStorage class serializes instances to a
     JSON file and deserializes JSON file to instances
     """
-
-    def __init__(self, file_path="file.json", objects={}):
-        self.file_path = file_path
-        self.objects = objects
-
-    @property
-    def file_path(self):
-        return self.__file_path
-
-    @file_path.setter
-    def file_path(self, file_path):
-        self.__file_path = file_path
-
-    @property
-    def objects(self):
-        return self.__objects
-
-    @objects.setter
-    def objects(self, objects):
-        self.__objects = objects
+    __file_path = 'file.json'
+    __objects = {}
 
     def all(self):
         """
         returns the dictionary __objects
         """
-        return self.__objects
+        return type(self).__objects
 
     def new(self, obj):
         """
@@ -49,20 +31,20 @@ class FileStorage:
         <obj class name>.id
         """
         key = type(obj).__name__ + '.' + obj.id
-        self.__objects[key] = obj.__dict__
+        type(self).__objects[key] = obj.__dict__
 
     def save(self):
         """
         serializes __objects to the JSON
         file (path: __file_path)
         """
-        objects = self.__objects
+        objects = type(self).__objects
         for _, val in objects.items():
             for k, v in val.items():
                 if type(v) == datetime:
                     val[k] = datetime.isoformat(v)
 
-        filename = self.__file_path
+        filename = type(self).__file_path
         with open(filename, 'w', encoding="utf-8") as f:
             json.dump(objects, f)
 
@@ -73,7 +55,7 @@ class FileStorage:
         otherwise, do nothing. If the file doesn’t
         exist, no exception should be raised)
         """
-        filename = self.__file_path
+        filename = type(self).__file_path
         if exists(filename):
             with open(filename, 'r+', encoding="utf-8") as f:
-                self.__objects = json.load(f)
+                type(self).__objects = json.load(f)
