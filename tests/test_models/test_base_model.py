ec55c3d3927bd361455dfeb76dbe043315a529ac
Thami <sainttsquared@gmail.com>
2022-12-12 04:03:39 +0200

    corrections

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,28 @@
-from models import base_model
+from models.base_model import BaseModel
 import unittest
+
 
 class TestBaseModel(unittest.TestCase):
     def test_save(self):
-        pass
+        basemodel = BaseModel()
+
+    def test_to_dict(self):
+        basemodel = BaseModel()
+        kind = type(basemodel.to_dict())
+        self.assertEqual(kind, dict)
+        base_dict = basemodel.to_dict()
+        created_at = type(base_dict['created_at'])
+        self.assertTrue( created_at == str)
+
+    def test_id(self):
+        basemodel = BaseModel()
+
+    def test_created_at(self):
+        basemodel = BaseModel()
+
+    def test_str(self):
+        basemodel = BaseModel()
+
+
+if __name__ == "__main__":
+    unittest.main()
